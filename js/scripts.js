adab53ee80a10d7b927ffbd80c50e03c49cf7003
Lindsey Raffa and Ronando Long <student@epicodus.com>
2016-01-13 17:10:46 -0800

    Refactor formula for grabbing inputs automatically.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,34 @@
 $(document).ready(function() {
   $("form#flavors").submit(function(event) {
-    var flavor1Input = $("input.flavor1").val();
-    var flavor2Input = $("input.flavor2").val();
-    var flavor3Input = $("input.flavor3").val();
 
-    var icecreams = [flavor1Input, flavor2Input, flavor3Input];
-                                      // icecreams.push(flavor1Input);
-                                      // icecreams.push(flavor2Input);
-                                      // icecreams.push(flavor3Input);
+    var iceCreams = [];
 
-    icecreams.forEach(function(cream) {
-      $("ul.list").prepend("<li>" + cream + "</li>")
+    $("input").each(function() {
+      var input = $(this).val();
+      iceCreams.push(input);
+    });
+
+    //
+    // for(var flavorNumber = 1; flavorNumber <= 3; flavorNumber += 1) {
+    //   var className = "input.flavor" + flavorNumber;
+    //   var input = $(className).val();
+    //   iceCreams.push(input);
+    // }
+
+    // var labels = ["flavor1", "flavor2", "flavor3"];
+    // labels.forEach(function(label) {
+    //   var className = "input." + label;  // first time thru className = "input.flavor1"
+    //   var input = $(className).val();
+    //   iceCreams.push(input);
+    // });
+
+    // var flavor1Input = $("input.flavor1").val();
+    // var flavor2Input = $("input.flavor2").val();
+    // var flavor3Input = $("input.flavor3").val();
+    // var iceCreams = [flavor1Input, flavor2Input, flavor3Input];
+
+    iceCreams.forEach(function(cream) {
+      $("ul.list").prepend("<li>" + cream + "</li>");
     });
 
     event.preventDefault();
